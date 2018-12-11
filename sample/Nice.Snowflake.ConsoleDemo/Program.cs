5370eac67730d5952fb55000182a4f42145dabe3
叶飞 <updateserver@163.com>
2018-12-11 23:41:00 +0800

    添加Snowflake的Demo

---
+++ sample/Nice.Snowflake.ConsoleDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Nice.Snowflake.ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var snowflake = new Snowflake(1, 1, 0);
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(snowflake.NextId());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
